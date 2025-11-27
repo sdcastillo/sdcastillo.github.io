e5dc0c601b5a5087148c61011344dabc46c504ae
Sam Castillo <castillo.sam.d@gmail.com>
2025-11-27 06:32:14 -0500

    Add files via upload

---
+++ predictiveinsights/GPT5queries.py
@@ -0,0 +1,21 @@
+#openaikey = "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"
+
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.responses.create(
+    #Read the official documentation for all the settings here!
+    model="gpt-5",
+    #Input text prompt here
+    #other formats, documents, files, that's for another time.
+    input="Help me do background on my ancestry.  I have a family tree that is series of 24 images of pdf pages (page_1.png ,,, page_24.png.  goes back hundreds of years, saved in the local direcotry as CASTILLO ILLING FAMILY TREE.PDF.  Using open source tools, help me draw or make a nested data format of the tree that i can use to visualize the tree.  i have python 3 installed on windows machine and tesseract 86 win. ",
+)
+
+# Print the result to the console
+print(response.output_text)
+
+# Write the result to a text file called "query_logs.txt"
+with open("query_logs.txt", "a", encoding="utf-8") as f:
+    f.write(response.output_text + "\n")
+
